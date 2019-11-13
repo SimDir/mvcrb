074847dc0b9be71ab08977517f99206df17667ef
diib <logic@xaker.ru>
2019-11-13 20:05:02 +0400

    исправлены некоторые функции

--- mvcrb/libs/View.php
+++ mvcrb/libs/View.php
@@ -2,6 +2,7 @@
 
 /**
  * Класс для работы с шаблонами
+ * 
  * view
  * 
  */
@@ -14,12 +15,8 @@ class View {
         $this->vars['headcssjs']='';
         $this->vars['bodycssjs']='';
     }
-    public function AddCss($stylesheet,$OnTop=true) {
-        if($OnTop){
-            $this->vars['headcssjs'].="<link rel=\"stylesheet\" href=\"$stylesheet\">".PHP_EOL;
-        }else{
-            $this->vars['bodycssjs'].="<link rel=\"stylesheet\" href=\"$stylesheet\">".PHP_EOL;
-        }
+    public function __invoke($val,$TplDir=false) {
+        return $this->execute($val,$TplDir);
     }
     public function __set($name, $value) {
         $this->vars[$name] = $value;
@@ -34,7 +31,19 @@ class View {
 //        return array();
         return FALSE;
     }
-
+    public function __isset($name) {
+        if (isset($this->vars[$name]) && !empty($this->vars[$name])) {
+            return true;
+        }
+        return false;
+    }
+    public function AddCss($stylesheet,$OnTop=true) {
+        if($OnTop){
+            $this->vars['headcssjs'].="<link rel=\"stylesheet\" href=\"$stylesheet\">".PHP_EOL;
+        }else{
+            $this->vars['bodycssjs'].="<link rel=\"stylesheet\" href=\"$stylesheet\">".PHP_EOL;
+        }
+    }
     public function VarGet($name) {
         if (isset($this->vars[$name])) {
 
@@ -50,12 +59,7 @@ class View {
         }
         
     }
-    public function __isset($name) {
-        if (isset($this->vars[$name]) && !empty($this->vars[$name])) {
-            return true;
-        }
-        return false;
-    }
+
 
     public function assign($name, $value) {
         if (isset($this->vars[$name]) && is_array($this->vars[$name])) {
