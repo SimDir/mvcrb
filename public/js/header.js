54eac960c1cd4eff62582bf2b61cc70f4cea26d0
sssavera <sssavera@users.noreply.github.com>
2019-12-04 11:24:21 +0400

    переписан скрипт главного меню теперь все четко2

--- public/js/header.js
+++ public/js/header.js
@@ -1,11 +1,5 @@
 'use strict';
 $( document ).ready(function() {    
-//    $('#HeaderMenuCross').click(function() {
-//        $('#HeaderMenuBurger').css({ "visibility": "visible", "transform": "rotate(180deg)" });
-//        $('#HeaderMenu').css({ "visibility": "hidden", "opacity": "0" });
-//        $('#HeaderMenuCross').css({ "visibility": "hidden", "transform": "rotate(0deg)" });
-//    });
-
     $('#HeaderMenuBurger').click(function() {
         $('#HeaderMenuBurger').css({ "visibility": "hidden", "transform": "rotate(0deg)" });
         $('#HeaderMenuCross').css({ "visibility": "visible", "transform": "rotate(180deg)" });
@@ -14,16 +8,10 @@ $( document ).ready(function() {
     $(document).click(function (e) {
         if ($(e.target).closest("#HeaderMenu").length)
             return;
-//        if ($(e.target).closest("#HeaderMenuCross").length)
-//            return;
         if ($(e.target).closest("#HeaderMenuBurger").length)
             return;
-//        console.log(e.target);
-//        e.stopPropagation();
-//        e.preventDefault();
         $('#HeaderMenuBurger').css({"visibility": "visible", "transform": "rotate(180deg)"});
         $('#HeaderMenu').css({"visibility": "hidden", "opacity": "0"});
         $('#HeaderMenuCross').css({"visibility": "hidden", "transform": "rotate(0deg)"});
-
     });
 });
