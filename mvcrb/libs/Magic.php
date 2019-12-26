7c4a1b35a35a5d5dce6720247ef2dfe0f992af8a
AsuRaHan <logic@xaker.ru>
2019-12-26 11:11:02 +0400

    редактор страниц и панель админки2

---
+++ mvcrb/libs/Magic.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace mvcrb;
+
+defined('ROOT') OR die('No direct script access.');
+/**
+ * Description of Magic
+ *
+ * @author Ivan P Kolotilkin
+ */
+class Magic {
+    //put your code here
+}
