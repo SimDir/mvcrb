882ea3fbdc59a8535645f134132d49db6b7bb96e
My Name <wizardhype1998@gmail.com>
2020-01-23 10:23:39 +0400

    адаптировал страницу О компании

--- public/js/header.js
+++ public/js/header.js
@@ -1,35 +1,9 @@
 'use strict';
 $( document ).ready(function() {    
-    // $('#HeaderMenuBurger').click(function() {
-    //     $('#HeaderMenuBurger').css({ "visibility": "hidden", "transform": "rotate(0deg)" });
-    //     $('#HeaderMenuCross').css({ "visibility": "visible", "transform": "rotate(180deg)" });
-    //     $('#HeaderMenu').css({ "visibility": "visible", "opacity": "1", "transform": "translateY(0)"});
-    // });
-    // $(document).click(function (e) {
-    //     if ($(e.target).closest("#HeaderMenu").length)
-    //         return;
-    //     if ($(e.target).closest("#HeaderMenuBurger").length)
-    //         return;
-    //     if($(document).width()>900)return;
-    //     $('#HeaderMenuBurger').css({"visibility": "visible", "transform": "rotate(180deg)"});
-    //     $('#HeaderMenu').css({"visibility": "hidden", "opacity": "0", "transform": "translateY(-100px)"});
-    //     $('#HeaderMenuCross').css({"visibility": "hidden", "transform": "rotate(0deg)"});
-    // });
-    // $('.header__menu_link').click(function(e) {
-    //     let parDiv = $(e.target);
-    //     let ImgCild = parDiv.children("img");
-    //     let DivCild = parDiv.children("div");
-    //     if(ImgCild.hasClass('MnuActiv')){
-    //         DivCild.removeClass("header_bottom_mobile").addClass("header_bottom");
-    //     }else{
-    //         DivCild.removeClass("header_bottom").addClass("header_bottom_mobile");
-    //     }
-    //     ImgCild.toggleClass('MnuActiv');
-    // });
-
     let burger = document.querySelector('.burger');
     let allLines = document.querySelectorAll('.burger span');
     let navBAr = document.querySelector('.header__menu');
+    let header = document.querySelector('header');
     let line = [];
     for(let elem of allLines) {
         line.push(elem);
@@ -54,6 +28,7 @@ $( document ).ready(function() {
             line[2].classList.add('line3-rotate');
             navBAr.style.left = '10%';
             navBAr.style.opacity = '1';
+            header.style.backgroundColor = "#3B487C";
         }
     };
 
