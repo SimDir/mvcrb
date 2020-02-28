9c7ea917b4c1e438204e7613723ddf86597b47d5
vladislav <wizardhype1998@gmail.com>
2020-02-28 18:50:35 +0400

    переверствывал сраница интренет агенства

--- public/js/script_digitalAgency_contacts.js
+++ public/js/script_digitalAgency_contacts.js
@@ -1,29 +1,27 @@
 'use strict';
-$( document ).ready(function() {  
-   $('#headerMoscow').click(function(){
-      $('#contactsUlsk').fadeOut();
-      $('#contactMoscow').fadeIn(1000);
-      $('#contactLiny').fadeOut();
-      $('#contactVilnus').fadeOut();
-
-      $('#headerUlsk').css({"borderBottom": "2px solid transparent", "fontWeight" : "normal"});
-      $('#headerMoscow').css({"borderBottom": "2px solid #fff", "fontWeight" : "bold"});
-      $('#headerLiny').css({"borderBottom": "2px solid transparent", "fontWeight" : "normal"});
-      $('#headerVilnus').css({"borderBottom": "2px solid transparent", "fontWeight" : "normal"});
-
-   });
-
-   $('#headerUlsk').click(function(){
-      $('#contactsUlsk').fadeIn(1000);
-      $('#contactMoscow').fadeOut();
-      $('#contactLiny').fadeOut();
-      $('#contactVilnus').fadeOut();
-
-      $('#headerUlsk').css({"borderBottom": "2px solid #fff", "fontWeight" : "bold"});
-      $('#headerMoscow').css({"borderBottom": "2px solid transparent", "fontWeight" : "normal"});
-      $('#headerLiny').css({"borderBottom": "2px solid transparent", "fontWeight" : "normal"});
-      $('#headerVilnus').css({"borderBottom": "2px solid transparent", "fontWeight" : "normal"});
-   });
-});
+document.addEventListener('DOMContentLoaded', function() {
+   let citiesItems = document.querySelectorAll('.cities__item');
+   let ulsk = document.querySelector('#contactsUlsk');
+   let moscow = document.querySelector('#contactMoscow');
+   for(let i=0; i<citiesItems.length; i++) {
+      
+      citiesItems[0].onclick = function() {
+         for(let i=0; i<citiesItems.length; i++) {
+            citiesItems[i].classList.remove('cities__item--active');
+         }
+         this.classList.add('cities__item--active');
+         ulsk.style.display = "block";
+         moscow.style.display = "none";
+      }
+      citiesItems[1].onclick = function() {
+         for(let i=0; i<citiesItems.length; i++) {
+            citiesItems[i].classList.remove('cities__item--active');
+         }
+         this.classList.add('cities__item--active');
+         ulsk.style.display = "none";
+         moscow.style.display = "block";
+      }
+   }
+})
 
 
