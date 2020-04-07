8ae1c2a85c991c2174a066e152caad5494fc8a9c
Vladislav <wizardhype1998@gmail.com>
2020-04-07 12:37:23 +0400

    верстаю страницу о доставке

--- public/js/script_stocks.js
+++ public/js/script_stocks.js
@@ -1,11 +1,11 @@
 'use strict';
 $( document ).ready(function() { 
-    let stocksOfferTitle = document.querySelectorAll('.stc-offer-title');
-    let stocksOfferList = document.querySelectorAll('.stc-offer-list');
+    let stocksOfferTitle = document.querySelectorAll('.stc-main-offer-title');
+    let stocksOfferList = document.querySelectorAll('.stc-main-offer-list--wrap');
     for(let i=0; i<stocksOfferTitle.length; i++) {
         stocksOfferTitle[i].onclick = function() {
             for(let i=0; i< stocksOfferList.length; i++) {
-                stocksOfferList[i].classList.toggle('stc-main__offer__list--active')
+                stocksOfferList[i].classList.toggle('stc-main-offer-list--active')
             }
         }
     }
