2f2a0ea92b9d9f85000fc770804cb3794ed7fd25
Vladislav <wizardhype1998@gmail.com>
2020-04-08 17:22:55 +0400

    начал делать квиз

--- public/js/script_stocks_complex.js
+++ public/js/script_stocks_complex.js
@@ -19,4 +19,27 @@ $( document ).ready(function() {
             stockHeader.classList.remove('stc-header-bgc--complex');
         }
     });
+
+
+    let quizStartImg = document.querySelector('.stc-main__quiz__start__img');
+    let quiz = document.querySelector('.stc-main__quiz');
+    let quizStartImgMain = document.querySelector('.stc-main__quiz__start__img-main')
+    quiz.addEventListener('mousemove', function(e) {
+        let amountMovedX = (e.pageX * -1 / 70);
+        quizStartImg.style.transform = 'translateX(' + amountMovedX + 'px)'; 
+        let mainAmountMovedX = (e.pageX * 1 / 70);
+        quizStartImgMain.style.transform = 'translateX(' + mainAmountMovedX + 'px)';
+    });
+
+
+
+    let quizStartBtn = document.querySelector('.stc-main__quiz__start__btn');
+    let quizStart = document.querySelector('.stc-main__quiz__start');
+    let quizStep = document.querySelector('.stc-main__quiz__step')
+    quizStartBtn.onclick = function(e) {
+        quizStart.style.transition = 'all 0.2s ease';
+        quizStart.style.transform = 'scale(0)';
+        quizStep.style.transition = 'all 0.2s ease 0.2s';
+        quizStep.style.transform = 'scale(1)';
+    }
 });
