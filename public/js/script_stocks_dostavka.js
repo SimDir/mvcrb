faf21fae42cee64cf88a8e4fd12ad34b09191aef
logic <logic@xaker.ru>
2020-04-10 11:00:39 +0400

    новые стили от владислава

---
+++ public/js/script_stocks_dostavka.js
@@ -0,0 +1,22 @@
+'use strict';
+$( document ).ready(function() { 
+    let stocksOfferTitle = document.querySelectorAll('.stc-main-offer-title');
+    let stocksOfferList = document.querySelectorAll('.stc-main-offer-list--wrap');
+    for(let i=0; i<stocksOfferTitle.length; i++) {
+        stocksOfferTitle[i].onclick = function() {
+            for(let i=0; i< stocksOfferList.length; i++) {
+                stocksOfferList[i].classList.toggle('stc-main-offer-list--active')
+            }
+        }
+    }
+
+    let stockHeader = document.querySelector('.stc-header');
+    window.addEventListener('scroll', function() {
+        if(pageYOffset > 0) {
+            stockHeader.classList.add('stc-header-bgc--dostavka');     
+        }
+        else {
+            stockHeader.classList.remove('stc-header-bgc--dostavka');
+        }
+    });
+});
